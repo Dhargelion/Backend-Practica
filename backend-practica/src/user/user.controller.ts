ae45d6e2f64ec72818bcbc9386d70f6f1d2cadf0
DiegoCV <corralesvdiego@gmail.com>
2020-10-14 18:11:02 -0400

    CRUD con nestjs y swagger

---
+++ backend-practica/src/user/user.controller.ts
@@ -0,0 +1,46 @@
+import { Controller, Post, Res, HttpStatus, Body, Get, Delete, Query, NotFoundException, Put } from '@nestjs/common';
+import { NOTFOUND } from 'dns';
+import { fromEvent } from 'rxjs';
+import {CreateUserDTO} from './dto/user.dto'
+import {UserService} from './user.service';
+
+@Controller('user')
+export class UserController {
+    constructor(private userService:UserService) {}
+    @Post('/create')
+    async createPost(@Res() res, @Body() createuserDTO : CreateUserDTO) {
+        const newUser = await this.userService.createUser(createuserDTO)
+        return res.status(HttpStatus.OK).json({
+            message: 'User Created',
+            user: newUser
+        });
+    }
+    @Get('/allUsers')
+    async getUser(@Res() res) {
+        const users = await this.userService.getUsers();
+        res.status(HttpStatus.OK).json({
+            message:'Users',
+            users: users
+        });
+    }
+    @Delete('/delete')
+    async deleteUser(@Res() res, @Query('userID') userID) {
+        const deletedUser = await this.userService.deleteUser(userID)
+        if(!deletedUser) throw new NotFoundException('User does not exits');
+        return res.status(HttpStatus.OK).json({
+            message:'user deleted',
+            deletedUser
+        });
+    }
+    @Put('/update')
+   async updateUser(@Res() res, @Body() createuserDTO:CreateUserDTO, @Query('userID') userID){
+        const updatedUser= await this.userService.updateUser(userID, createuserDTO);
+        if(!updatedUser) throw new NotFoundException('user does not exist');
+        return res.status(HttpStatus.OK).json({
+            message:'user updated',
+            updatedUser
+        });
+    }
+    
+
+}
---
+++ backend-practica/src/user/user.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import {Model} from 'mongoose'
+import { from } from 'rxjs';
+import {InjectModel} from '@nestjs/mongoose'
+import {user} from './interfaces/user.interface'
+import {CreateUserDTO} from './dto/user.dto'
+
+
+
+@Injectable()
+export class UserService {
+    constructor(@InjectModel('User') private userModel: Model<user>) {
+    }
+
+    async getUsers(): Promise<user[]> {
+        const users = await this.userModel.find()
+        return users;
+
+    }
+    async createUser( createUserDTO :CreateUserDTO): Promise<user> {
+        const user = new this.userModel(createUserDTO);
+        return user.save();
+    }
+    async deleteUser(UserID: string): Promise<user> {
+        const delUser= await this.userModel.findByIdAndDelete(UserID);
+        return delUser;
+    }
+    async updateUser(userID: string, createUserDTO:CreateUserDTO): Promise <user> {
+        const updatedUser = await this.userModel.findByIdAndUpdate(userID, createUserDTO, {new:true});
+        return updatedUser;
+    }
+}
